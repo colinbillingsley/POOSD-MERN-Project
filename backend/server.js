74932993d19058c4427a299069269fbe1f952a63
Jack Carr <ja588361@ucf.edu>
2023-11-26 20:08:55 -0500

    hosting changes

--- backend/server.js
+++ backend/server.js
@@ -23,9 +23,12 @@ app.use((req, res, next) => {
   next()
 })
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 // routes
 app.use('/api/user', userRoutes)
 app.use('/api/recipes', recipeRoutes)
+
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
